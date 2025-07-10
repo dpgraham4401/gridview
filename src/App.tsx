452f693a05e635e9d48939ee384609083e9d0872
David Graham <dpgraham4401@gmail.com>
2025-07-10 18:57:54 -0400

    refactor static header content into separate react component

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@
  */
 import './App.css'
 import {GridMap} from "@/components/GridMap.tsx";
-
+import {Header} from "@/components/Header.tsx";
 
 
 function App() {
@@ -12,14 +12,10 @@ function App() {
 
     return (
         <div className="min-h-screen w-screen bg-gray-100 dark:bg-gray-900 transition-colors p-2">
-            <header className="w-full max-w-xl mx-auto mb-4">
-                <h1 className="text-xl font-bold text-gray-800 dark:text-gray-100 mb-2">Power Grid Explorer</h1>
-                <p className="text-gray-600 dark:text-gray-300">
-                    Explore the components of the US power grid and conduct power flow analysis using the interactive map below.
-                </p>
-            </header>
+            <Header/>
             <div className="flex flex-col items-center justify-center">
-                <div className="bg-white dark:bg-gray-800 rounded-xl shadow-lg p-6 w-11/12 h-[500px] md:h-[600px] xl:h-[700px] flex flex-col items-center transition-colors">
+                <div
+                    className="bg-white dark:bg-gray-800 rounded-xl shadow-lg p-6 w-11/12 h-[500px] md:h-[600px] xl:h-[700px] flex flex-col items-center transition-colors">
                     <GridMap mapKey={mapTilerKey}/>
                 </div>
             </div>
