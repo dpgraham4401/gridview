dff6543b1bf66a2fd12628fa12c85657b39bb817
David Paul Graham <dpgraham4401@gmail.com>
2025-07-10 18:14:41 -0400

    add VITE_MAPTILLER_API_KEY to vite-end.d.ts

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,14 @@
 /// <reference types="vite/client" />
+
+interface ViteTypeOptions {
+  // disallow unknown environment variable keys.
+  strictImportMetaEnv: unknown
+}
+
+interface ImportMetaEnv {
+    readonly VITE_MAPTILER_API_KEY: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
