a4293762b40b84c52e30d1924843fbf1a67b13ee
David Paul Graham <dpgraham4401@gmail.com>
2025-07-10 18:49:35 -0400

    POC addind power grid components to the map viewer

--- src/components/GridMap.tsx
+++ src/components/GridMap.tsx
@@ -3,9 +3,9 @@
  * Uses {@link https://github.com/Leaflet/Leaflet Leaflet } under the hood,
  * which is well-maintained, relatively lightweight, good for 2D maps, should be mobile-friendly.
  */
-import {MapContainer, TileLayer} from "react-leaflet";
+import {MapContainer, TileLayer, GeoJSON} from "react-leaflet";
 import {getMapBaseLayer, type MapBaseLayerName} from "@/lib/map.ts";
-import {useRef, useState} from "react";
+import {useEffect, useRef, useState} from "react";
 import 'leaflet/dist/leaflet.css';
 const latitude = 42.3555;
 const longitude = -75.0602;
@@ -17,10 +17,33 @@ interface GridMapProps {
 export function GridMap({mapKey}: GridMapProps) {
     const [baseLayer] = useState<MapBaseLayerName>(mapKey ? 'mapTiler' : 'openStreetMap');
     const mapRef = useRef(null);
-    const mapTileLayerProps = getMapBaseLayer(baseLayer, mapKey)
+    const mapTileLayerProps = getMapBaseLayer(baseLayer, mapKey);
+    const [geoJson, setGeoJson] = useState<any>(null);
+
+    useEffect(() => {
+        fetch('/sample-grid.geojson')
+            .then(res => res.json())
+            .then(setGeoJson);
+    }, []);
+
+    function onEachFeature(feature: any, layer: any) {
+        if (feature.properties) {
+            let popupContent = '';
+            if (feature.properties.type === 'substation') {
+                popupContent = `<b>Substation:</b> ${feature.properties.name}`;
+            } else if (feature.properties.type === 'transmission') {
+                popupContent = `<b>Transmission Line:</b> ${feature.properties.name}`;
+            }
+            layer.bindPopup(popupContent);
+        }
+    }
+
     return (
         <MapContainer center={[latitude, longitude]} zoom={7} ref={mapRef} className="w-full h-full">
             <TileLayer {...mapTileLayerProps} />
+            {geoJson && (
+                <GeoJSON data={geoJson} onEachFeature={onEachFeature} />
+            )}
         </MapContainer>
     )
 }
