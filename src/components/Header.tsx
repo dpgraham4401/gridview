64a1ca8534c7b08828efa63eaab504c336d0f66d
David Graham <dpgraham4401@gmail.com>
2025-07-11 15:01:26 -0400

    add short explaination in header about the GeoJSON format

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,11 +5,19 @@
 
 export function Header() {
     return (
-        <header className="w-full max-w-xl mb-4 p-5">
+        <header className=" mb-4 p-5">
             <h1 className="text-xl font-bold text-gray-800 dark:text-gray-100 mb-2">Power Grid Explorer</h1>
-            <p className="text-gray-600 dark:text-gray-300">
+            <p className="text-gray-600 dark:text-gray-300 max-w-prose">
                 Explore the components of the US power grid and conduct power flow analysis using the interactive
                 map below.
+            </p>
+            <br/>
+            <p className="text-gray-600 dark:text-gray-300 max-w-prose">
+                This default power grid is supplied for easy viewing, but you can also upload your own GeoJSON files.
+                For more information on the data format, download the
+                <a href="/public/example-grid.json" className="dark:text-cyan-400 text-cyan-800">
+                    {' '}Example Grid GeoJSON
+                </a>
             </p>
         </header>
     )
