1854f828cca3b1385c7bb6102cb43b513429b2de
David Graham <dpgraham4401@gmail.com>
2025-07-15 13:49:22 -0400

    remove unused ref

--- src/components/GeoJsonUploader.tsx
+++ src/components/GeoJsonUploader.tsx
@@ -2,15 +2,11 @@
  * Component for uploading and parsing GeoJSON files.
  */
 
-import {useRef} from "react";
-
 interface GeoJsonUploaderProps {
     onChange: (geoJson: any) => void;
 }
 
 export function GeoJsonUploader({onChange}: GeoJsonUploaderProps) {
-    const inputRef = useRef<HTMLInputElement>(null);
-
 
     return (
         <div className="mb-3 md:max-w-1/3 px-3">
@@ -20,7 +16,6 @@ export function GeoJsonUploader({onChange}: GeoJsonUploaderProps) {
             >Upload file</label
             >
             <input
-                ref={inputRef}
                 onChange={onChange}
                 className="relative m-0 block w-full min-w-0 flex-auto cursor-pointer rounded border border-solid border-secondary-500 bg-transparent bg-clip-padding px-3 py-[0.32rem] text-base font-normal leading-[2.15] text-surface transition duration-300 ease-in-out file:-mx-3 file:-my-[0.32rem] file:me-3 file:cursor-pointer file:overflow-hidden file:rounded-none file:border-0 file:border-e file:border-solid file:border-inherit file:bg-transparent file:px-3  file:py-[0.32rem] file:text-surface focus:border-primary focus:text-gray-700 focus:shadow-inset focus:outline-none dark:border-white/70 dark:text-white  file:dark:text-white"
                 id="formFileLg"
