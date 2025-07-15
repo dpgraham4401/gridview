1f27a834fcfc3ee551fe7786d9da0c804cca6c91
David Graham <dpgraham4401@gmail.com>
2025-07-15 13:38:22 -0400

    fix eslint config structure

--- eslint.config.js
+++ eslint.config.js
@@ -3,27 +3,26 @@ import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
-import { globalIgnores } from 'eslint/config'
+import {globalIgnores} from 'eslint/config'
 
 export default tseslint.config([
-  globalIgnores(['dist']),
-  {
-    files: ['**/*.{ts,tsx}'],
-    extends: [
-      js.configs.recommended,
-      reactHooks.configs['recommended-latest'],
-      reactRefresh.configs.vite,
-    ],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
-    },
-  },
-],
+    globalIgnores(['dist']),
     {
-      extends : [tseslint.configs.recommended],
-      rules: {
-        '@typescript-eslint/no-explicit-any': 'off'
-      }
-
-    })
+        files: ['**/*.{ts,tsx}'],
+        extends: [
+            js.configs.recommended,
+            reactHooks.configs['recommended-latest'],
+            reactRefresh.configs.vite,
+        ],
+        languageOptions: {
+            ecmaVersion: 2020,
+            globals: globals.browser,
+        },
+    },
+    {
+        extends: [tseslint.configs.recommended],
+        rules: {
+            '@typescript-eslint/no-explicit-any': 'off'
+        }
+    }
+])
